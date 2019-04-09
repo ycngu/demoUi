75eab2c393a6e1a25c568fb1844e87c24da94061
ycngu <snl123698745@163.com>
2019-04-09 14:48:31 +0800

    将svg代码分离出去变为icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+    props:['name']
+};
+</script>
+
+<style lang="scss">
+        .g-icon {
+            width: 1.3em;
+            height: 1.3em;
+        }
+</style>
+
